cea1b6feb6f5366c1aeb9b95bc59f8e95e286209
cafeamanda <cafeamanda@linuxnewz.com>
2016-04-02 16:29:08 -0300

    Angular Scope and new button

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,7 @@ var gulp = require('gulp'),
     changed = require('gulp-changed'),
     rev = require('gulp-rev'),
     browserSync = require('browser-sync'),
+    ngannotate = require('gulp-ng-annotate'),
     del = require('del');
 
 gulp.task('jshint', function() {
@@ -28,7 +29,7 @@ gulp.task('usemin', ['jshint'], function() {
   return gulp.src('./app/menu.html')
   .pipe(usemin({
     css:[minifycss(),rev()],
-    js:[uglify(),rev()]
+    js:[ngannotate(),uglify(),rev()]
   }))
   .pipe(gulp.dest('dist/'));
 });
